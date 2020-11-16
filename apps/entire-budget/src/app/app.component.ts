5ca0ca60b11029d7537eb385c9fa89fd9bfbf36a
Chandler Baskins <baskinschandler@gmail.com>
2020-11-16 16:16:23 -0600

    init commit

--- apps/entire-budget/src/app/app.component.ts
+++ apps/entire-budget/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Message } from '@entire-budget/api-interfaces';
 
 @Component({
   selector: 'entire-budget-root',
@@ -8,6 +6,4 @@ import { Message } from '@entire-budget/api-interfaces';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  hello$ = this.http.get<Message>('/api/hello');
-  constructor(private http: HttpClient) {}
 }
