94f76dd4b41c3f2f3a1d2e055cb7a32b2755116e
LAPTOP-TAR46155\Chandler Baskins <baskinschandler@gmail.com>
2021-01-06 22:08:50 -0600

    getting closer

--- apps/entire-budget/src/app/pages/budget/budget.service.ts
+++ apps/entire-budget/src/app/pages/budget/budget.service.ts
@@ -48,13 +48,15 @@ export class BudgetService {
     tap((v) => console.log('THe final VALue', v)),
     shareReplay(1)
   );
-  selectedLineItemAction = new Subject<LineItem>();
+  private selectedLineItemAction = new Subject<LineItem>();
 
   selectedLineItem$ = combineLatest([
     this.crudBudgetGroups$,
     this.selectedLineItemAction,
   ]).pipe(
     map(([budgetGroup, selectedItem]) => {
+      if (!selectedItem) return null;
+      //is this really neccessary
       const selectedGroup = budgetGroup.find(
         (group) => group.id === selectedItem.budgetGroupId
       );
@@ -64,8 +66,8 @@ export class BudgetService {
       return selectedLineItem;
     }),
     distinctUntilChanged(),
-    tap(console.log),
-    shareReplay()
+    shareReplay(),
+    tap(console.log)
   );
 
   //Support Methods
